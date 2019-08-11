4f37f52c4a9ae64ed27f704fe8a94aa8c856b1b3
ncleguizamon <ncleguizamon3@misena.edu.co>
2019-08-11 11:49:45 -0500

    create connect mongo db mongose and dotenv

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+mongoose.connect(process.env.MONDO_URI, {
+        userNewUrlParser: true
+    }).then(() => console.log('DB connected'))
+    .catch(err => console.error(err))
 
 const server = new ApolloServer({
     typeDefs,
